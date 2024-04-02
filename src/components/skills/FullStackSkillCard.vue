3f2cd374b1bd7704bbc0e2672f5c7b17adaf6ef8
AJ Vancattenburch <wanderwithme1988@gmail.com>
2024-04-02 14:32:26 -0600

    Created FullStackSkillCard.vue

--- src/components/skills/FullStackSkillCard.vue
+++ src/components/skills/FullStackSkillCard.vue
@@ -1,5 +1,38 @@
 <template>
-  <div>
-    
+  <div v-if="skill.category === 'Full Stack'" class="card d-flex flex-column justify-content-center align-items-center mb-2">
+    <h6 class="card-title text-center pt-2"><span class="emphasize-title">Learn</span> {{ skill.name }}</h6>
+    <div class="img-container d-flex justify-content-center align-items-center rounded-1">
+      <img :src="skill.picture" :alt="`Picture of ${skill.name}`" :title="`Click button to purchase for ${skill.upgradeQuantityReq} ${skill.name} upgrades`" class="card-img-top img-fluid">
+    </div>
+    <small class="sub-text">*3 {{ skill.name }} upgrades required*</small>
+    <i class="mdi mdi-battery"> <span class="upgrade-requirements">{{ skill.upgradeQuantityReq }}</span></i>
   </div>
 </template>
+
+<script>
+import { Skill } from "../../models/Skill.js";
+
+export default {
+  props: {
+    skill: {
+      type: Skill,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.mdi {
+  color: green;
+  .upgrade-requirements {
+    color: red;
+    font-style: normal;
+  }
+}
+.sub-text {
+  font-size: 0.75rem;
+  font-weight: 500;
+  font-style: italic;
+}
+</style>
