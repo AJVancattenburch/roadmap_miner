31488081335357e92277cb47613665be6dd26352
AJ Vancattenburch <wanderwithme1988@gmail.com>
2024-04-02 16:07:12 -0600

    cleanup state in MilestoneState.js

--- src/state/scopedStates/MilestoneState.js
+++ src/state/scopedStates/MilestoneState.js
@@ -1,44 +1,50 @@
 import { reactive } from "vue";
 import { Milestone } from "../../models/Milestone.js";
 
+const ranks = reactive([
+  /** @type {import('../../models/Milestone.js').Milestone[]} */
+  new Milestone({
+    id: '1',
+    name: 'Junior Developer',
+    description: 'You are now a Junior Developer!',
+    knowledgeThreshold: 1000,
+    picture: 'https://placehold.it/200x200',
+    multiplier: 1,
+    bonus: 0
+  }),
+  new Milestone({
+    id: '2',
+    name: 'Mid-Level Developer',
+    description: 'You are now a Mid-Level Developer!',
+    knowledgeThreshold: 5000,
+    picture: 'https://placehold.it/200x200',
+    multiplier: 2,
+    bonus: 0
+  }),
+  new Milestone({
+    id: '3',
+    name: 'Senior Developer',
+    description: 'You are now a Senior Developer!',
+    knowledgeThreshold: 10000,
+    picture: 'https://placehold.it/200x200',
+    multiplier: 3,
+    bonus: 0
+  }),
+  new Milestone({
+    id: '4',
+    name: 'Lead Developer',
+    description: 'You are now a Lead Developer!',
+    knowledgeThreshold: 20000,
+    picture: 'https://placehold.it/200x200',
+    multiplier: 4,
+    bonus: 0
+  })
+]);
+
 export const milestoneState = reactive({
   /** @type {import('../../models/Milestone.js').Milestone[]} */
-  rank: [
-    new Milestone({
-      id: '1',
-      name: 'Junior Developer',
-      description: 'You are now a Junior Developer!',
-      knowledgeThreshold: 1000,
-      picture: 'https://placehold.it/200x200',
-      multiplier: 1,
-      bonus: 0
-    }),
-    new Milestone({
-      id: '2',
-      name: 'Mid-Level Developer',
-      description: 'You are now a Mid-Level Developer!',
-      knowledgeThreshold: 5000,
-      picture: 'https://placehold.it/200x200',
-      multiplier: 2,
-      bonus: 0
-    }),
-    new Milestone({
-      id: '3',
-      name: 'Senior Developer',
-      description: 'You are now a Senior Developer!',
-      knowledgeThreshold: 10000,
-      picture: 'https://placehold.it/200x200',
-      multiplier: 3,
-      bonus: 0
-    }),
-    new Milestone({
-      id: '4',
-      name: 'Lead Developer',
-      description: 'You are now a Lead Developer!',
-      knowledgeThreshold: 20000,
-      picture: 'https://placehold.it/200x200',
-      multiplier: 4,
-      bonus: 0
-    })
-  ]
+    ranks,
+
+  /** @type {import('../../models/Milestone.js').Milestone|null} */
+    activeRank: null
 });
