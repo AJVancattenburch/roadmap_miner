3d4a877adea04bd9279922d8ee7587fdd047b21a
AJ Vancattenburch <wanderwithme1988@gmail.com>
2024-04-02 14:31:40 -0600

    Updated FrontEndSkillCard.vue

--- src/components/skills/FrontEndSkillCard.vue
+++ src/components/skills/FrontEndSkillCard.vue
@@ -2,7 +2,7 @@
   <div v-if="skill.category === 'Front End'" class="card d-flex flex-column justify-content-center align-items-center mb-2">
     <h6 class="card-title text-center pt-2"><span class="emphasize-title">Learn</span> {{ skill.name }}</h6>
     <div class="img-container d-flex justify-content-center align-items-center rounded-1">
-      <img :src="skill.picture" :alt="`Picture of ${skill.name}`" :title="`Click button to purchase ${skill.name} for ${skill.upgradeQuantityReq}`" class="card-img-top img-fluid">
+      <img :src="skill.picture" :alt="`Picture of ${skill.name}`" :title="`Click button to purchase for ${skill.upgradeQuantityReq} ${skill.name} upgrades`" class="card-img-top img-fluid">
     </div>
     <small class="sub-text">*3 {{ skill.name }} upgrades required*</small>
     <i class="mdi mdi-battery"> <span class="upgrade-requirements">{{ skill.upgradeQuantityReq }}</span></i>
