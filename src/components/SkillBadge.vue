4c483df7ba0fd05095ee8373d67f322347985f5e
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2024-04-12 13:57:40 -0600

    skillbadge styles

--- src/components/SkillBadge.vue
+++ src/components/SkillBadge.vue
@@ -1,11 +1,11 @@
 <template>
-  <div v-if="skill.isUnlocked" class="col-12 card d-flex flex-row justify-content-center align-items-center">
-    <div class="col-4 img-container d-flex align-items-center">
+  <div v-if="skill.isUnlocked" class="flex-row card d-flex justify-content-center align-items-center">
+    <div class="col-4 img-container d-flex">
       <img :src="skill.picture" :alt="`Picture of ${skill.picture} Mastery Badge`" :class="`${nameSlug}-img img-fluid`">
     </div>
-    <div class="col-8 info-container d-flex flex-column justify-content-center align-items-center">
-      <small class="card-title green d-flex flex-column fw-bold">{{ skill.name }} Mastery</small>
-      <small class="card-text px-1">{{ skill.description }}</small>
+    <div class="col-8 info-container d-flex flex-column align-items-center">
+      <small class="card-title green fw-bold">{{ skill.name }} Mastery</small>
+      <small class="card-text">{{ skill.description }}</small>
     </div>
   </div>
 </template>
@@ -39,19 +39,23 @@ export default {
   overflow: hidden;
   .img-fluid {
     width: 75%;
-    height: 75%;
+    height: 80%;
     background: var(--blue-radial-gradient);
     padding-block: 0.25rem;
   }
   .info-container {
     position: relative;
     top: 0;
+    left: -15px;
     .card-title, .card-text {
       font-size: 0.75rem;
+      line-height: 0.75rem;
     }
     .card-title {
       margin-top: 0.5rem;
       line-height: 0.75rem;
+      justify-content: flex-start;
+      align-items: flex-start;
     }
     .card-text {
       margin-bottom: 0.5rem;
