10025239423e8748b42c874b8858192c4d7b3279
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2024-04-12 15:43:16 -0600

    done styling skillbadge.vue for now

--- src/components/SkillBadge.vue
+++ src/components/SkillBadge.vue
@@ -1,9 +1,9 @@
 <template>
   <div v-if="skill.isUnlocked" class="flex-row card d-flex justify-content-center align-items-center">
-    <div class="col-4 img-container d-flex">
+    <div class="col-4 img-container d-flex me-1">
       <img :src="skill.picture" :alt="`Picture of ${skill.picture} Mastery Badge`" :class="`${nameSlug}-img img-fluid`">
     </div>
-    <div class="col-8 info-container d-flex flex-column align-items-center">
+    <div class="col-8 info-container d-flex flex-column">
       <small class="card-title green fw-bold">{{ skill.name }} Mastery</small>
       <small class="card-text">{{ skill.description }}</small>
     </div>
@@ -36,6 +36,7 @@ export default {
 .card {
   width: 100%;
   height: 3.5rem;
+  background: var(--glass-red-linear-gradient);
   overflow: hidden;
   .img-fluid {
     width: 75%;
@@ -48,14 +49,15 @@ export default {
     top: 0;
     left: -15px;
     .card-title, .card-text {
+      color: #fafafa;
+      text-shadow: 0 1px 2px #000;
       font-size: 0.75rem;
       line-height: 0.75rem;
     }
     .card-title {
-      margin-top: 0.5rem;
+      margin-block: 0.25rem;
       line-height: 0.75rem;
       justify-content: flex-start;
-      align-items: flex-start;
     }
     .card-text {
       margin-bottom: 0.5rem;
